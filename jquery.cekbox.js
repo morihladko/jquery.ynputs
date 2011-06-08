68e09c5c480ab7f8e555909a1a7de170a9e59446
Peter Morihladko <morihladko@gmail.com>
2011-06-08 13:32:10 +0200

    Massive rewrite, added minified version
    Selekt now has tabindex, handle keyboard inputs like up, down,
    enter, jumping to item by a letter.
    The code of selekt has now an OOP design.
    Added minified versions (with yuicompressor-2.4.6) and a version
    which inlude both cekbox and selekt

--- jquery.cekbox.js
+++ jquery.cekbox.js
@@ -1,28 +1,16 @@
-/*
- * Copyright (c) 2010 Peter Morihladko <morihladko@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 (function( $ ) {
 
-	$.fn.cekbox = function() {
+	function change( $cekbox, $checkbox ) {
+		if( $checkbox.is(':checked') ) {
+			$cekbox.addClass('unchecked').removeClass('checked');
+			$checkbox.attr('checked', false); 
+		} else {
+			$cekbox.addClass('checked').removeClass('unchecked')
+			$checkbox.attr('checked', true);
+		}
+	}
 
+	$.fn.cekbox = function() {
 		return $(this).each( function() {
 			var $checkbox = $(this);
 
@@ -31,32 +19,30 @@
 			}
 
 			$checkbox.hide();
-
-			var $cekbox = $("<span class='cekbox' />");
-
-			if( $checkbox.is(':checked') ) {
-				$cekbox.addClass( 'checked' );
-			} else {
-				$cekbox.addClass( 'unchecked' );
+			if( ! $checkbox.attr('tabindex') ) {
+				$checkbox.attr('tabindex',	0);
 			}
 
-			if( $checkbox.is(':disabled') ) {
-				$cekbox.addClass( 'disabled' );
+			var $cekbox = $("<span class='cekbox' />")
+				.attr('tabindex', $checkbox.attr('tabindex'));
+
+			if( $checkbox.is(':checked') ) {
+				$cekbox.addClass('checked');
+			} else {
+				$cekbox.addClass('unchecked');
 			}
 
 			$checkbox.after( $cekbox );
 
-			$cekbox.click( function() {
-				if( $cekbox.is('.disabled') ) {
-					return;
-				}
-
-				if( $checkbox.is(':checked') ) {
-					$cekbox.addClass( 'unchecked' ).removeClass('checked');
-					$checkbox.attr( 'checked', false ); 
-				} else {
-					$cekbox.addClass( 'checked' ).removeClass('unchecked')
-					$checkbox.attr( 'checked', true );
+			$cekbox.click( function(ev) {
+				change($cekbox, $checkbox);
+				ev.preventDefault();
+			} )
+			.keydown( function(ev) {
+				// spacebar
+				if( ev.keyCode == 32 ) { 
+					change($cekbox, $checkbox);
+					ev.preventDefault();
 				}
 			} );
 
--- jquery.ynputs.js
+++ jquery.ynputs.js
@@ -336,3 +336,63 @@
 	} ;
 
 })( jQuery );
+(function( $ ) {
+
+	function change( $cekbox, $checkbox ) {
+		if( $checkbox.is(':checked') ) {
+			$cekbox.addClass('unchecked').removeClass('checked');
+			$checkbox.attr('checked', false); 
+		} else {
+			$cekbox.addClass('checked').removeClass('unchecked')
+			$checkbox.attr('checked', true);
+		}
+	}
+
+	$.fn.cekbox = function() {
+		return $(this).each( function() {
+			var $checkbox = $(this);
+
+			if( ! $checkbox.is('input[type="checkbox"]') ) {
+				return;
+			}
+
+			$checkbox.hide();
+			if( ! $checkbox.attr('tabindex') ) {
+				$checkbox.attr('tabindex',	0);
+			}
+
+			var $cekbox = $("<span class='cekbox' />")
+				.attr('tabindex', $checkbox.attr('tabindex'));
+
+			if( $checkbox.is(':checked') ) {
+				$cekbox.addClass('checked');
+			} else {
+				$cekbox.addClass('unchecked');
+			}
+
+			$checkbox.after( $cekbox );
+
+			$cekbox.click( function(ev) {
+				change($cekbox, $checkbox);
+				ev.preventDefault();
+			} )
+			.keydown( function(ev) {
+				// spacebar
+				if( ev.keyCode == 32 ) { 
+					change($cekbox, $checkbox);
+					ev.preventDefault();
+				}
+			} );
+
+			$checkbox.change( function() {
+				if( $checkbox.is(':checked') && $cekbox.is(':not(.checked)') ) {
+					$cekbox.addClass( 'checked' ).removeClass('unchecked');
+
+				} else if( $checkbox.is(':not(:checked)') && $cekbox.is(':not(.unchecked)') ) {
+					$cekbox.addClass('unchecked').removeClass('checked');
+				}
+			} ); 
+		} );
+	}
+
+})( jQuery );
